02e6e456ed63652a7c48527101a488065bfa1b42
Paul Swartz <paul@paulswartz.net>
2021-06-17 14:13:31 -0400

    fix: stop using deprecated Supervisor code

--- lib/crawler/supervisor.ex
+++ lib/crawler/supervisor.ex
@@ -1,15 +1,9 @@
 defmodule Crawler.Supervisor do
   alias Crawler.Link.Registry
-  use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, [])
-  end
-
-  @impl true
-  def init(_opts) do
-    children = [worker(Registry, [[]])]
-
-    supervise(children, strategy: :one_for_one)
+    Supervisor.start_link([
+      Registry
+    ], strategy: :one_for_one)
   end
 end
