ebb47a396a9cf01ef3f98529f2e0204d8c372fee
Josh Larson <jlarson@mbta.com>
2025-05-01 12:57:52 -0500

    chore: Allow this library to be used when Elixir version is 1.14 (#13)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Crawler.Mixfile do
     [
       app: :crawler,
       version: "0.2.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
